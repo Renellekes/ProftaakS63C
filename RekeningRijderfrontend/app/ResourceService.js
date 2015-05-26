9c4702e647e7a773fb8353b3d6b10f23ca683550
rick963 <rick963@users.noreply.github.com>
2015-05-26 10:24:13 +0200

    rijderwebsite

---
+++ RekeningRijderfrontend/app/ResourceService.js
@@ -0,0 +1,19 @@
+var restservice = angular.module('restservice', ['ngResource']);
+
+restservice.factory('Cartraker', ['$resource', function ($resource) {
+        return $resource('http://localhost:24707/Rekeningadministratie/api/RekAdmin',
+        null, 
+        {
+            'query': {method: 'GET', isArray: true, url: 'getAllCars'},
+            'getFactuurs':{method: 'GET', isArray: true, url: 'getAllFactuur'}
+        });
+    }]);
+
+restservice.factory('restService', ['$resource', function ($resource) {
+        return $resource('http://localhost:24707/Rekeningadministratie/api/RekAdmin',
+        null, 
+        {
+            'getAlleKilometerTarieven': {method: 'GET', isArray: true, url: '/KilometerTarieven/All'},
+            'getKilometerTarief':{method: 'GET', isArray: true, url: '/KilometerTarieven/:id'}
+        });
+    }]);
---
+++ RekeningRijderfrontend/app/factuurview/factuurview.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var rekadmin = angular.module('myApp.view1', ['ngRoute']);
+
+rekadmin.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl'
+        });
+    }]);
+
+rekadmin.controller('View1Ctrl', function ($scope, Cartraker, $http) {
+    $scope.cars = [];
+    $scope.init = function ()
+    {
+        $scope.cars = Cartraker.query();
+    };
+
+    $scope.list = [];
+
+    $scope.submit = function () {
+        if ($scope.kenteken) {
+            var Auto = {
+                kenteken: $scope.kenteken,
+                voertuig: $scope.voertuig,
+                eerstekleur: $scope.eerstekleur,
+                zitplaatsen: $scope.aantalzitplaatsen
+            };
+            var res = $http.post('http://localhost:24707/Rekeningadministratie/api/RekAdmin/addCartraker', Auto);
+            res.success(function (data, status, headers, config) {
+                $scope.cars = Cartraker.query();
+            });
+            $scope.list.push(Auto.kenteken + Auto.voertuig + Auto.eerstekleur + Auto.zitplaatsen);
+            $scope.kenteken = '';
+            $scope.voertuig = '';
+            $scope.eerstekleur = '';
+            $scope.aantalzitplaatsen = '';
+
+        }
+    };
+});
