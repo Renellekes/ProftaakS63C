6f1bd1747d1763d5ec368a8a13650c2fc3988676
rick963 <rick963@users.noreply.github.com>
2015-05-27 12:12:46 +0200

    factuur update

--- RekeningRijderfrontend/app/ResourceService.js
+++ RekeningRijderfrontend/app/ResourceService.js
@@ -1,10 +1,18 @@
 var restservice = angular.module('restservice', ['ngResource']);
 
 restservice.factory('Cartraker', ['$resource', function ($resource) {
-        return $resource('http://localhost:24707/Rekeningadministratie/api/RekAdmin',
+        return $resource('http://localhost:8080/RekeningAdministratieBackend/api/RekAdmin',
         null, 
         {
-            'query': {method: 'GET', isArray: true, url: 'getAllCars'},
-            'getFactuurs':{method: 'GET', isArray: true, url: 'getAllFactuur'}
+            'query': {method: 'GET', isArray: true, url: 'http://localhost:8080/RekeningAdministratieBackend/api/RekAdmin/getAllCars'},
+            'getFactuurs':{method: 'GET', isArray: true, url: 'http://localhost:8080/RekeningAdministratieBackend/api/RekAdmin/getAllFactuur'}
         });
     }]);
+//restservice.factory('restService', ['$resource', function ($resource) {
+//        return $resource('http://localhost:47010/Rekeningadministratie/api/RekAdmin',
+//        null, 
+//        {
+//            'getAlleFacturen': {method: 'GET', isArray: true, url: 'http://localhost:47010/Rekeningadministratie/api/RekAdmin/KilometerTarieven/All'}
+//            
+//        });
+//    }]);
