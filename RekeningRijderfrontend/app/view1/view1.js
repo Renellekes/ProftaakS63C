d954709eacdb81f84b2b6477933ba4fe52477ff0
rick963 <rick963@users.noreply.github.com>
2015-05-27 14:54:18 +0200

    web update

--- RekeningRijderfrontend/app/view1/view1.js
+++ RekeningRijderfrontend/app/view1/view1.js
@@ -17,6 +17,22 @@ rekadmin.controller('View1Ctrl', function ($scope, Cartraker, $http) {
         //$scope.options = [{ id: 1, kenteken: "1-gha-23", zitplaats: "5", kleur: "paars", voertuig: "beta"}, { id: 2, kenteken: "23-hah-7", zitplaats: "3", kleur: "groen", voertuig: "alfa" }];
         $scope.selectedOption = $scope.cars[0];
     };
-
-    $scope.list = [];    
+ $scope.modifyCar = function () {
+     
+             var Auto = {
+                id: $scope.cars.id,
+                kenteken: $scope.cars.kenteken,
+                eigenaar: $scope.cars.eigenaar,
+                gestolen: $scope.cars.gestolen,
+                voertuig: $scope.cars.voertuig,
+                eersteKleur: $scope.kleur,
+                zitplaaten: $scope.cars.zitplaaten
+            };
+            $scope.list = Auto[0];  
+            var res = $http.post('http://localhost:8080/RekeningAdministratieBackend/api/RekAdmin/modifyCar', Auto);
+            res.success(function (data, status, headers, config) {
+                $scope.cars = Cartraker.query();
+            });
+    };
+      
 });
