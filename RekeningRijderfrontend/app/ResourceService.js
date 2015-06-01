d612ab31c4f464a312fcf42ee18775d3b9c12793
rick963 <rick963@users.noreply.github.com>
2015-06-01 16:52:56 +0200

    Merge branch 'master' of https://github.com/Renellekes/ProftaakS63C
    Conflicts:
    	RekeningAdmfrontend/nbproject/private/private.xml

--- RekeningRijderfrontend/app/ResourceService.js
+++ RekeningRijderfrontend/app/ResourceService.js
@@ -1,11 +1,12 @@
 var restservice = angular.module('restservice', ['ngResource']);
 
-restservice.factory('Cartraker', ['$resource', function ($resource) {
+restservice.factory('Cartracker', ['$resource', function ($resource) {
         return $resource('http://localhost:8080/RekeningAdministratieBackend/api/RekAdmin',
         null, 
         {
             'query': {method: 'GET', isArray: true, url: 'http://localhost:8080/RekeningAdministratieBackend/api/RekAdmin/getAllCars'},
-            'getFactuurs':{method: 'GET', isArray: true, url: 'http://localhost:8080/RekeningAdministratieBackend/api/RekAdmin/getAllFactuur'}
+            'getFactuurs':{method: 'GET', isArray: true, url: 'http://localhost:8080/RekeningAdministratieBackend/api/RekAdmin/getAllFactuur'},
+            'getCar':{method: 'GET', isArray: true, url: 'http://localhost:8080/RekeningAdministratieBackend/api/RekAdmin/Car/:id'}
         });
     }]);
 restservice.factory('restService', ['$resource', function ($resource) {
