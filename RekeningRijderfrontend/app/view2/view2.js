1a560edf5b2b3c1ff490708ab8bc8505a8f5e3d6
rick963 <rick963@users.noreply.github.com>
2015-06-01 13:53:34 +0200

    rekeningrijder spul

--- RekeningRijderfrontend/app/view2/view2.js
+++ RekeningRijderfrontend/app/view2/view2.js
@@ -11,11 +11,40 @@ rekadmin.config(['$routeProvider', function ($routeProvider) {
 
 rekadmin.controller('View2Ctrl', function ($scope, restService, $http) {
     $scope.eigenaars = [];
+    var bewaarEigenaarInfo = null;
     $scope.init = function ()
     {
-       //$scope.eigenaar = [{naam:'hans', adres:"ja", woonplaats:"nee"}];
        $scope.eigenaars = restService.getEigenaars();
         $scope.owner = $scope.eigenaar[0];
         
     };
+    
+     $scope.update = function () {       
+         
+        $scope.id = $scope.selectedOption.id;        
+        $scope.adres = $scope.selectedOption.adres;
+        $scope.woonplaats = $scope.selectedOption.woonplaats;        
+        bewaarEigenaarInfo = $scope.selectedOption;
+    };
+    
+ $scope.submit = function () {
+        if ($scope.adres && $scope.woonplaats) {
+            var Eigenaar = {
+                id: bewaarEigenaarInfo.id,
+                woonplaats: $scope.woonplaats,
+                adres: $scope.adres               
+            };
+            var res = $http.post('http://localhost:8080/RekeningAdministratieBackend/api/RekAdmin/modifyEigenaar', Eigenaar);
+            res.success(function (data, status, headers, config) {
+                $scope.eigenaars = restService.getEigenaars();
+            });
+            $scope.selectedOption = '';            
+            bewaarEigenaarInfo = null;
+            $scope.woonplaats = '';
+            $scope.adres = '';     
+        
+
+        }
+    };            
+    
 });
