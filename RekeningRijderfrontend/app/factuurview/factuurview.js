74cda9ef8578afaf088fee4e87568d75c5602192
rick963 <rick963@users.noreply.github.com>
2015-06-02 10:48:36 +0200

    Merge origin/master
    Conflicts:
    	RekeningAdmfrontend/app/ResourceService.js

--- RekeningRijderfrontend/app/factuurview/factuurview.js
+++ RekeningRijderfrontend/app/factuurview/factuurview.js
@@ -17,6 +17,7 @@ rekadmin.controller('factuurviewCtrl', function ($scope, Cartracker, $http) {
         
         //$scope.facturen = [{ id: 1, kenteken: "1-gha-23", zitplaats: "5", kleur: "paars", voertuig: "beta"}, { id: 2, kenteken: "23-hah-7", zitplaats: "3", kleur: "groen", voertuig: "alfa" }];
        
-    };
+    };   
+    
   
 });
--- RekeningRijderfrontend/app/voertuigen/voertuigen.js
+++ RekeningRijderfrontend/app/voertuigen/voertuigen.js
@@ -7,14 +7,14 @@ rekadmin.config(['$routeProvider', function ($routeProvider) {
             templateUrl: 'voertuigen/voertuigen.html',
             controller: 'voertuigenCtrl'
         });
-
     }]);
-rekadmin.controller('voertuigenCtrl', function ($scope, Cartraker, $http) {
+
+rekadmin.controller('voertuigenCtrl', function ($scope, Cartracker, $http) {
     $scope.cars = [];
     var bewaarAutoInfo = null;
     $scope.init = function ()
     {
-        $scope.cars = Cartraker.query();
+        $scope.cars = Cartracker.query();
         //$scope.options = [{ id: 1, kenteken: "1-gha-23", zitplaats: "5", kleur: "paars", voertuig: "beta"}, { id: 2, kenteken: "23-hah-7", zitplaats: "3", kleur: "groen", voertuig: "alfa" }];
         $scope.selectedOption = $scope.cars[0];
     };
